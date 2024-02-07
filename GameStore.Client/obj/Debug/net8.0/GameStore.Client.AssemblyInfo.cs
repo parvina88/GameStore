b2ce41f7257fbeb7c2d99e2c8ee9ea27e726d558
Parvina Zulfikorova <parvina@silkroadprofessionals.com>
2024-02-07 13:09:29 +0500

    .

--- GameStore.Client/obj/Debug/net8.0/GameStore.Client.AssemblyInfo.cs
+++ GameStore.Client/obj/Debug/net8.0/GameStore.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c2865884fe3916367e64fcb09312edfbefbf003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f7e7d177c6bddcb54d7ee149a54100b8e2cd6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
